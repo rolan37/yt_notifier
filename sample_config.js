4f9142dfe351a979e7e649f0e7af269e492dd3ca
Rolan Pereira <59693138+rolan37@users.noreply.github.com>
2024-09-10 23:21:38 +0530

    Update and rename config.js to sample_config.js

---
+++ sample_config.js
@@ -0,0 +1,10 @@
+// config.js
+module.exports = {
+    YOUTUBE_API_KEY: '',   // Add your own API here directly or configure a .env fule
+    CHANNEL_IDS: [
+        'UCpa54N4vmqqUKkGJcSV62Lw',  // 20thCenturyStudios
+        'UC9YHyj7QSkkSg2pjQ7M8Khg',  // paramountmovies
+        'UCq0OueAsdxH6b8nyAspwViw',  // UniversalPictures
+        'UCu-cVVMn41qUA-pwdEhNqhg'   // WarnerBrosPicturesLA
+    ] 
+};
