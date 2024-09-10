430a7890f41257230b73e8f9d4d32afdeb271303
Rolan Pereira <59693138+rolan37@users.noreply.github.com>
2024-09-10 23:22:27 +0530

    Update sample_config.js
    Added a few sample channel IDs for testing the application

--- sample_config.js
+++ sample_config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    YOUTUBE_API_KEY: '',   // Add your own API here directly or configure a .env fule
+    YOUTUBE_API_KEY: '',   // Add your own API here
     CHANNEL_IDS: [
         'UCpa54N4vmqqUKkGJcSV62Lw',  // 20thCenturyStudios
         'UC9YHyj7QSkkSg2pjQ7M8Khg',  // paramountmovies
